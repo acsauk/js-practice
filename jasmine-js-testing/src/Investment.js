10042b8b4ff4e9f4de43238ac3c6c3fee51973ff
acsauk <acsauk@gmail.com>
2016-10-09 18:50:32 +0100

    passes sixth test for isGood investment

--- jasmine-js-testing/src/Investment.js
+++ jasmine-js-testing/src/Investment.js
@@ -8,4 +8,8 @@ function Investment (params) {
   Investment.prototype.roi = function () {
     return (this.stock.sharePrice - this.sharePrice) / this.sharePrice;
   };
+
+  Investment.prototype.isGood = function () {
+    return this.roi() > 0;
+  };
 };
