4ac1ed694a1625a3efc2170a4e3b2863335eb71e
acsauk <acsauk@gmail.com>
2016-10-09 14:46:22 +0100

    first failing test

--- jasmine-js-testing/spec/InvestmentSpec.js
+++ jasmine-js-testing/spec/InvestmentSpec.js
@@ -1,6 +1,7 @@
 describe("Investment", function() {
   // Given an investment, it should be of a stock
   it("should be of a stock", function() {
-    
+    var investment = new Investment();
+    expect(investment.stock).toBe(stock);
   });
 });
