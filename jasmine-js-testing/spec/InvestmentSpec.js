5b904ccf91554f041847ab0808c69a35c7e98c24
acsauk <acsauk@gmail.com>
2016-10-09 18:04:47 +0100

    implements and passes second test for share quantity. Refactors parameters of investment to be stored in params object and refactors test to use beforeEach

--- jasmine-js-testing/spec/InvestmentSpec.js
+++ jasmine-js-testing/spec/InvestmentSpec.js
@@ -1,8 +1,33 @@
+"use strict";
+
 describe("Investment", function() {
+
+var stock, investment;
+
+  beforeEach(function() {
+    stock = new Stock();
+    investment = new Investment({
+      stock: stock,
+      shares: 100
+    });
+  });
   // Given an investment, it should be of a stock
   it("should be of a stock", function() {
-    var stock = new Stock();
-    var investment = new Investment(stock);
+    // var stock = new Stock();
+    // var investment = new Investment({
+    //   stock: stock,
+    //   shares: 100
+    // });
     expect(investment.stock).toBe(stock);
+  });
+
+  // Given an investment, it should have the invested shares quantity
+  it("should have the invested shares quantity", function() {
+    // var stock = new Stock();
+    // var investment = new Investment({
+    //   stock:  stock,
+    //   shares: 100
+    // });
+    expect(investment.shares).toEqual(100);
   });
 });
